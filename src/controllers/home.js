b5db9fa4b9428e967626534c5e749295c0ae0e93
fiat <114096819+fiatdl@users.noreply.github.com>
2023-11-24 12:18:39 +0700

    Add files via upload
    initiate controller

---
+++ src/controllers/home.js
@@ -0,0 +1,145 @@
+const hotel = require("../models/product.model");
+const type = require("../models/type.model");
+const collection = require("../models/collection.model");
+class Home {
+    type(req, res, next) {
+
+        let logged;
+        if (req.cookies.token) {
+            logged = true;
+        }
+        else {
+            logged = false;
+        }
+        hotel.find({ type: req.params.type }).then((list) => {
+            list = list.map((item) => item.toObject());
+
+            type.find({}).then((e) => {
+                e = e.map((i) => i.toObject());
+                res.render("home", { list, islogged: logged, e });
+
+            }).catch(err => { console.log(err) });
+
+        });
+
+
+
+    }
+    index(req, res, next) {
+
+        let start = req.query.start || "";
+        let end = req.query.end || "";
+        let quantity = req.query.quantity || 1;
+        let search = req.query.search || "";
+        let logged;
+
+        if (req.cookies.token) {
+            logged = true;
+        }
+        else {
+            logged = false;
+        }
+        // if (start === "" && end === "" && quantity === "" && search === "") {
+        //     hotel.find({}).then((list) => {
+        //         list = list.map((item) => item.toObject());
+
+        //         type.find({}).then((e) => {
+        //             e = e.map((i) => i.toObject());
+        //             res.render("home", { list, islogged: logged, e });
+
+        //         }).catch(err => { console.log(err) });
+
+        //     });
+        // }
+
+
+        const day1 = (start !== "") ? new Date(start) : Date.now();
+        const day2 = (end !== "") ? new Date(end) : Date.now();
+        if (search !== "") {
+
+            hotel
+                .find({
+                    name: { $regex: "^" + search, $options: "i" }, startday: {
+                        $lte: day1,
+                    },
+                    endday: {
+                        $gte: day2,
+                    },
+                    maximuncus: {
+                        $gte: quantity
+                    }
+                })
+                .then((list) => {
+                    list = list.map((item) => item.toObject());
+
+                    type.find({}).then((e) => {
+                        e = e.map((i) => i.toObject());
+                        collection.find({ user: req.cookies.id }).then(wish => {
+                            wish = wish.map((i) => i.toObject());
+                            let name = req.cookies.username;
+                            let email = req.cookies.email;
+                            let phone = req.cookies.phone;
+                            let avatar = req.cookies.avatar;
+                            res.render("home", { name, email, phone, avatar, list, islogged: logged, e, wish });
+                        }).catch(err => console.log(err));
+
+                    }).catch(err => console.log(err));
+                })
+        }
+        else {
+            hotel
+                .find({
+
+                    endday: {
+                        $gte: day2,
+                    },
+                    maximuncus: {
+                        $gte: quantity
+                    }
+                })
+                .then((list) => {
+                    list = list.map((item) => item.toObject());
+
+                    type.find({}).then((e) => {
+                        e = e.map((i) => i.toObject());
+                        collection.find({ user: req.cookies.id }).then(wish => {
+                            wish = wish.map((i) => i.toObject());
+                            let name = req.cookies.username;
+                            let email = req.cookies.email;
+                            let phone = req.cookies.phone;
+                            let avatar = req.cookies.avatar;
+                            res.render("home", { name, email, phone, avatar, list, islogged: logged, e, wish });
+                        }).catch(err => console.log(err));
+
+                    }).catch(err => console.log(err));
+                })
+        }
+
+        // if (Object.keys(req.query).length === 0) {
+        //     hotel.find({}).then((list) => {
+        //         list = list.map((item) => item.toObject());
+
+        //         res.render("home", { list });
+        //     });
+        // }
+        // else {
+        //     hotel
+        //         .find({
+        //             maximuncus: { $gte: quantity },
+        // startday: {
+        //     $lte: day1,
+        // },
+        // endday: {
+        //     $gte: day2,
+        // },
+        //         })
+        //         .then((list) => {
+        //             list = list.map((item) => item.toObject());
+
+        //             res.render("home", { list, admin: true, admin: isAdmin });
+        //         });
+        // }
+
+    }
+}
+module.exports = new Home();
---
+++ src/controllers/login.js
@@ -0,0 +1,105 @@
+const user = require("../models/users.model");
+var jwt = require("jsonwebtoken");
+var Cookies = require("cookies-js");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+class LoginController {
+  index(req, res, next) {
+    res.render("login", { loginform: true, addProcessing: true });
+
+  }
+  googleauth(req, res, next) {
+
+    user.find({ email: req.cookies.userProfile.email }).then((docs) => {
+      if (docs.length === 0) {
+        const NewUser = new user({
+
+          email: req.cookies.userProfile.email,
+          fullName: req.cookies.userProfile.name,
+          role: "user",
+        });
+
+        NewUser.save();
+        const token = jwt.sign({ name: NewUser.email }, "fiat");
+        res.cookie("id", NewUser._id);
+        res.cookie("token", token);
+        res.cookie("avatar", NewUser.avatar);
+        res.cookie("username", NewUser.fullName);
+        res.cookie("role", NewUser.role);
+        res.cookie("email", NewUser.email);
+        res.redirect("/");
+      }
+      else {
+
+
+        const token = jwt.sign({ name: req.cookies.userProfile.email }, "fiat");
+        res.cookie("id", docs[0]._id);
+        res.cookie("token", token);
+        res.cookie("avatar", docs[0].avatar);
+        res.cookie("username", docs[0].fullName);
+        res.cookie("role", docs[0].role);
+        res.cookie("email", docs[0].email);
+        res.cookie("phone", docs[0].phoneNumber);
+        if (docs[0].avatar === undefined) {
+          res.redirect("/user/setavatar");
+        } else {
+          res.redirect("/");
+        }
+
+      }
+    });
+  }
+
+  async checkEmail(req, res, next) {
+    await user.find({ email: req.body.username }).then((docs) => {
+      if (docs.length === 0) {
+        res.render("login", {
+          message: "tài khoản chưa  được đăng kí",
+          announce: true,
+          addProcessing: true
+        });
+      }
+    });
+    next();
+
+  }
+
+  login(req, res, next) {
+    user
+      .findOne({
+        email: req.body.username,
+      })
+      .then((data) => {
+
+        if (data) {
+          bcrypt.compare(req.body.password, data.password).then(function (result) {
+            if (result) {
+              const token = jwt.sign({ name: req.body.username }, "fiat");
+              res.cookie("id", data._id);
+              res.cookie("token", token);
+              res.cookie("avatar", data.avatar);
+              res.cookie("username", data.fullName);
+              res.cookie("password", data.password);
+              res.cookie("role", data.role);
+              if (data.avatar === undefined) {
+                res.redirect("/user/setavatar");
+              } else {
+                res.redirect("/");
+              }
+            } else {
+              res.render("login", {
+                message: "sai mật khẩu",
+                announce: true,
+                addProcessing: true
+              });
+            }
+          });
+        } else {
+          res.render("login");
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+}
+module.exports = new LoginController();
