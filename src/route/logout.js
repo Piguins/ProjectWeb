12e81bec547261563cb4d409e4ae8f5063d2a707
kiethongngu <21522263@gm.uit.edu.vn>
2023-12-07 11:38:30 +0700

    trip UI

--- src/route/logout.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const LogOutController = require("../controllers/logout");
-
-router.get("/", LogOutController.index);
-
-module.exports = router;
--- src/route/product.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Route = express.Router();
-const ProductController = require("../controllers/product")
-
-
-Route.get("/:id", ProductController.specific);
-Route.post("/payment", ProductController.payment);
-Route.get("/payment/success", ProductController.success);
-module.exports = Route;
