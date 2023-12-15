f9e2d44178d72d4ab7fe13c0a6a71be36aec31eb
kiethongngu <21522263@gm.uit.edu.vn>
2023-12-15 11:02:07 +0700

    Login control

---
+++ src/controllers/login.js
@@ -0,0 +1,52 @@
+const user = require("../models/users.model");
+var jwt = require("jsonwebtoken");
+var Cookies = require("cookies-js");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+class LoginController {
+  index(req, res, next) {
+    res.render("login", { loginform: true, addProcessing: true });
+
+  }
+  googleauth(req, res, next) {
+
+    user.find({ email: req.cookies.userProfile.email }).then((docs) => {
+      if (docs.length === 0) {
+        const NewUser = new user({
+
+          email: req.cookies.userProfile.email,
+          fullName: req.cookies.userProfile.name,
+          role: "user",
+        });
+
+        NewUser.save();
+        const token = jwt.sign({ name: NewUser.email }, "fiat");
+        res.cookie("id", NewUser._id);
+        res.cookie("token", token);
+        res.cookie("avatar", NewUser.avatar);
+        res.cookie("username", NewUser.fullName);
+        res.cookie("role", NewUser.role);
+        res.cookie("email", NewUser.email);
+        res.redirect("/");
+      }
+      else {
+
+
+        const token = jwt.sign({ name: req.cookies.userProfile.email }, "fiat");
+        res.cookie("id", docs[0]._id);
+        res.cookie("token", token);
+        res.cookie("avatar", docs[0].avatar);
+        res.cookie("username", docs[0].fullName);
+        res.cookie("role", docs[0].role);
+        res.cookie("email", docs[0].email);
+        res.cookie("phone", docs[0].phoneNumber);
+        if (docs[0].avatar === undefined) {
+          res.redirect("/user/setavatar");
+        } else {
+          res.redirect("/");
+        }
+
+      }
+    });
+  }
